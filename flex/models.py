e641198a43348a89082502ddfec7c3d49adbe307
Regina <qregi.1@outlook.com>
2020-08-24 17:22:59 +0100

    added flex app

---
+++ flex/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
+from streams.blocks import CardBlock
+
+
+class FlexPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(icon="title", null=True, blank=True)),
+        ('image', ImageChooserBlock(required=False)),
+        ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['h3', 'h4', 'centre', 'bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link', 'embed'],  null=True, blank=True)),
+        ('embed', EmbedBlock(icon="media", null=True, blank=True)),
+        ('embed_HTML', blocks.RawHTMLBlock(required=False, help_text="use this to embed elements that do not embed using the normal embed block, e.g. google forms", null=True, blank=True)),
+        ('cards', CardBlock()),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("body", classname="full"),
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
