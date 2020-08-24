522ca4c415c15a017e5dde581789ddcf795c2063
Regina <qregi.1@outlook.com>
2020-08-24 17:26:51 +0100

    updated base.py with new apps

--- GirlsInSpace/settings/base.py
+++ GirlsInSpace/settings/base.py
@@ -26,6 +26,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
+    'flex',
+    'streams',
+    'menus',
     
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
