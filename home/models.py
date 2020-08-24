800621eb41ffdc06b6c002c246088c83cd6ebac0
Regina <qregi.1@outlook.com>
2020-08-24 17:24:37 +0100

    altered homepage app

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.embeds.blocks import EmbedBlock
 
 
 class HomePage(Page):
-    pass
+
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title", icon="title")),
+        ('paragraph', blocks.RichTextBlock(icon="pilcrow")),
+        ('embed', EmbedBlock(icon="media")),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body', classname="full"),
+    ]
