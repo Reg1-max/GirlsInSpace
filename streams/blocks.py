90259d9e59376563db44abc2ff29a46aed2ef63d
Regina <qregi.1@outlook.com>
2020-08-28 10:55:40 +0100

    making text and image on cards optional (request)

--- streams/blocks.py
+++ streams/blocks.py
@@ -24,9 +24,9 @@ class CardBlock(blocks.StructBlock):
     cards = blocks.ListBlock(
         blocks.StructBlock(
             [
-               ('image', ImageChooserBlock(required=True)),
+               ('image', ImageChooserBlock(required=False)),
                ('title', blocks.CharBlock(required=True, max_length=40)),
-               ('text', blocks.TextBlock(required=True, max_length=200)),
+               ('text', blocks.TextBlock(required=False, max_length=200)),
                ('button_page', blocks.PageChooserBlock(required=False)),
                ('button_url', blocks.URLBlock(required=False, help_text="use if you want to go to external links")),
                ('button_title', blocks.CharBlock(required=False, max_length=40, help_text="Give your button a title")),
