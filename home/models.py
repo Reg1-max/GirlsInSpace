ac0202175683d332a35d5e6fa8d40b8800c15336
Regina <qregi.1@outlook.com>
2020-08-28 09:12:46 +0100

    adding styling and background img to homepage

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -14,6 +15,14 @@ class HomePage(Page):
         ('paragraph', blocks.RichTextBlock(icon="pilcrow")),
         ('embed', EmbedBlock(icon="media")),
     ], null=True, blank=True)
+
+    background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        related_name="background_image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('body', classname="full"),
